e0a7b56f8c504bbad1d63dad9afa413dc6dcda10
smin95 <seung.min@mail.mcgill.ca>
2020-12-10 17:47:24 -0500

    finished ch3

--- copy_paste.R
+++ copy_paste.R
@@ -1,4 +1,4 @@
-### Question 2
+### Question 2 {-}
 
 <div class="question">
 With the CLT distribution, we obtained a p-value smaller than 0.05 and with the t-distribution, one that is larger. They can't both be right. What best describes the difference?
